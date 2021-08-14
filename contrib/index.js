9855143a0bc7d51bfd90ffe5c34e6073ecaf81fa
avindra <avindra@users.noreply.github.com>
2021-08-14 21:43:59 +0000

    deploy: e5407320f4fc5b040b9cbed40b5c5349b0183b02

--- contrib/index.js
+++ contrib/index.js
@@ -3,22 +3,45 @@ import {loadDocument, loadPage} from './../util.js';
 const CSS_GALLERY = `.mw-contributions-list`;
 const CSS_NEXT = ".mw-nextlink";
 
+const DEFAULT_LIMIT = 50;
+
+function createLimit() {
+	const l = document.createElement('select');
+	[20,50,100,250,500].forEach(lim => {
+		const o = document.createElement('option');
+		o.value = lim;
+		o.text = lim;
+		l.appendChild(o);
+	});
+	l.value = DEFAULT_LIMIT;
+	return l;
+}
+
 export const register = () => {
 	const nav = document.querySelector(".mw-pager-navigation-bar");
 	if (!nav) return;
 
+	let limit = DEFAULT_LIMIT;
 	let nextDocument = document;
 	let nextPage = nextDocument.querySelector(CSS_NEXT);
 
 	if (nextPage) {
 		const ctr = document.createElement('button');
 		ctr.textContent = 'Older';
-		nav.parentNode.prepend(ctr);
+		const P = nav.parentNode;
+		const L = createLimit();
+		L.onchange = (event) => {
+			limit = Number(event.target.value);
+		}
+		P.prepend(L);
+		P.prepend(ctr);
 
 		ctr.onclick = async () => {
-			nextDocument = await loadDocument(nextPage.href);
+			ctr.disabled = true;
+			nextDocument = await loadDocument(nextPage.href + `&limit=${limit}`);
 			nextPage = nextDocument.querySelector(CSS_NEXT);
 			loadPage(nextDocument, CSS_GALLERY);
+			ctr.disabled = false;
 		}
 	}
 }
